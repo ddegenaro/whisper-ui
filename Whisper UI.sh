e22f1157edb7ac836a5e64967c788933652389ab
Dan DeGenaro <daniel.degenaro@gmail.com>
2025-01-27 00:11:36 -0500

    Minor fixes

--- Whisper UI.sh
+++ Whisper UI.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
-# Unix start script for Whisper-UI
+# Check if the script is executable and prompt to make it so if not
+if [ ! -x "$0" ]; then
+    echo "This script is not executable. Making it executable now..."
+    chmod +x "$0"
+    echo "Re-run the script now: ./$0"
+    exit
+fi
 
-echo Preparing Whisper-UI. Do not close this window...
+echo "Preparing Whisper-UI. Do not close this window..."
 
-# check if pip package whisper-ui is installed or not:
+# Check if the pip package 'whisper-ui' is installed
+pip show whisper-ui > /dev/null 2>&1
 
-pip show whisper-ui >/dev/null 2>&1
-
-# if not installed, install it:
-
+# If not installed, install it
 if [ $? -ne 0 ]; then
-    echo Installing Python library...
+    echo "Installing Python library..."
     pip install whisper-ui
 fi
 
-# run whisper-ui avoid opening a terminal window:
+# Run whisper-ui in the background, avoid opening a terminal window
+echo "Starting Whisper-UI..."
+nohup pythonw -m whisper_ui > /dev/null 2>&1 &
 
-echo Starting Whisper-UI...
-pythonw -m whisper_ui
-
-# exit the script:
-
+# Exit the script
 exit
